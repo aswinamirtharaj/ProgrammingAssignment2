49dff610de35a9e9452898f692845e78aae8b35e
aswinamirtharaj <aswin.amirtharaj@yahoo.com>
2016-06-19 20:24:54 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We are caching the result of a matrix inverse to avoid recomputing everytime and 
+## resue the value by retrieving from cache.
+## makeCacheMatrix -- take a matrix as input and returns the get and set in a list
+## cacheSolve -- take the value returned by makeCacheMatrix and calculates the inverse matrix 
+## for first time and stores in cache when called. From second time it uses the value stored in cache.
 
-## Write a short comment describing this function
+## Storing the matrix and setter, getter: retuning a list
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of the matrix first time and stores in cache and later retrieves
 
 cacheSolve <- function(x, ...) {
        m <- x$getInverse()
